a261bd79fc306300f1e0222c2d7a79efba6d5e40
vika <gabaibaibayok@gmail.com>
2025-02-25 22:44:27 +0500

    updated main

--- main.py
+++ main.py
@@ -3,11 +3,11 @@ import logging
 import sys
 from os import getenv
 
-from aiogram import Bot, Dispatcher, html
+from aiogram import Bot, Dispatcher, html, types, F
 from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
 from aiogram.filters import CommandStart, Command, BaseFilter
-from aiogram import types
+from aiogram.fsm.context import FSMContext
 from aiogram.utils.keyboard import InlineKeyboardBuilder, ReplyKeyboardBuilder, KeyboardBuilder, InlineKeyboardButton
 from aiogram.utils.formatting import Bold
 from aiogram.fsm.state import State, StatesGroup
@@ -32,11 +32,13 @@ class TeamSubmissions(StatesGroup):
 
 
 @dp.message(CommandStart())
-async def start(message: types.Message):
+async def start(message: types.Message, state: FSMContext):
+    await state.set_state(TeamSubmissions.name)
+    print(state)
     builder = ReplyKeyboardBuilder()
     for n in team_names:
         builder.add(types.KeyboardButton(text=n))
-    
+    await message.answer("Welcome! Submit photos with people you find here")
     await message.answer("What team are you?", reply_markup=builder.as_markup(resize_keyboard=True))
 
 
@@ -46,7 +48,10 @@ class TeamNameFilter(BaseFilter):
     async def __call__(self, message: types.Message) -> bool:
         return message.text in team_names and len(message.text) == 1
 @dp.message(TeamNameFilter())
-async def cmd_start(message: types.Message):
+async def cmd_start(message: types.Message, state: FSMContext):
+    await state.update_data(name=message.text)
+    await state.set_state(TeamSubmissions.letter)
+    print(state)
     builder = ReplyKeyboardBuilder()
     for i in letters:
         builder.add(types.KeyboardButton(text=i))
@@ -60,11 +65,21 @@ async def cmd_start(message: types.Message):
 class LetterFilter(BaseFilter):
     async def __call__(self, message: types.Message) -> bool:
         return message.text in letters and len(message.text) == 1
-    
 @dp.message(LetterFilter())
-async def echo_handler(message: types.Message):
+async def echo_handler(message: types.Message, state: FSMContext):
+    await state.set_state(TeamSubmissions.submission)
+    await state.update_data(letter=message.text)
+
     letter = message.text
     await message.answer(f"Send photo with a person whose name starts with *{letter}*\ ", parse_mode="MarkdownV2")
+
+@dp.message(F.photo)
+async def handle_photo(message: types.Message, state: FSMContext):
+    print("User sent a photo!") 
+
+    data = await state.get_data()
+    print(data["name"], data["letter"])
+    await state.clear()
     
 
 
